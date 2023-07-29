57f0c9d543776668f99f727e4aded913421cd6c3
Mursalin Kabir <mail2mursalin@gmail.com>
2023-07-30 01:14:13 +0900

    creating user model with getter and setter

---
+++ live/src/main/java/com/mursalinkabir/live/user/User.java
@@ -0,0 +1,40 @@
+package com.mursalinkabir.live.user;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    //it will handle auto increment
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
